997eeb7e37d013d3e3f8f08e414c8e0b81565008
Robert McDonnell <rcmcd@hotmail.com>
2020-10-28 23:15:45 -0600

    Switched to Parallel.ForEach example

--- ParallelTasks/Program.cs
+++ ParallelTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,23 +7,21 @@ namespace ParallelTasks
 {
     class Program
     {
-        static void Task1()
+        static void WorkOnItem(object item)
         {
-            Console.WriteLine("Task 1 starting");
-            Thread.Sleep(2000);
-            Console.WriteLine("Task 1 ending");
-        }
-
-        static void Task2()
-        {
-            Console.WriteLine("Task 2 starting");
-            Thread.Sleep(1000);
-            Console.WriteLine("Task 2 ending");
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
         }
 
         static void Main(string[] args)
         {
-            Parallel.Invoke(() => Task1(), () => Task2());
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item =>
+            {
+                WorkOnItem(item);
+            });
+
             Console.WriteLine("Finished processing. Press a key to end.");
             Console.ReadKey();
         }
