0b7b533f9f76c4bfe85d56f0bf0b747c865a829a
Robert McDonnell <rcmcd@hotmail.com>
2020-11-08 17:55:07 -0700

    Added Collections project. Added MusicTracks project

---
+++ Collections/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "the", "quick", "brown", "fox", "jumps", "over", "the", "dog" };
+            LinkedList<string> sentence = new LinkedList<string>(words);
+
+            Display(sentence, "The linked list values:");
+            Console.WriteLine("sentance.Contains(\"jumps\") = {0}", sentence.Contains("jumps"));
+
+            sentence.AddFirst("today");
+            Display(sentence, "Test 1: Add 'today' to the beginning of the list:");
+
+            LinkedListNode<string> mark1 = sentence.First;
+            sentence.RemoveFirst();
+            sentence.AddLast(mark1);
+            Display(sentence, "Test 2: Move first node to be last node:");
+
+
+
+            // Create a new sorted list of strings, with string
+            // keys.
+            SortedList<string, string> openWith =
+                new SortedList<string, string>();
+
+            // Add some elements to the list. There are no
+            // duplicate keys, but some of the values are duplicates.
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+            // The Add method throws an exception if the new key is
+            // already in the list.
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+
+        }
+
+        private static void Display(LinkedList<string> words, string test)
+        {
+            Console.WriteLine(test);
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void IndicateNode(LinkedListNode<string> node, string test)
+        {
+            Console.WriteLine(test);
+            if (node.List == null)
+            {
+                Console.WriteLine("Node '{0}' is not in the list.\n",
+                    node.Value);
+                return;
+            }
+
+            StringBuilder result = new StringBuilder("(" + node.Value + ")");
+            LinkedListNode<string> nodeP = node.Previous;
+
+            while (nodeP != null)
+            {
+                result.Insert(0, nodeP.Value + " ");
+                nodeP = nodeP.Previous;
+            }
+
+            node = node.Next;
+            while (node != null)
+            {
+                result.Append(" " + node.Value);
+                node = node.Next;
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
+
+    }
+}
